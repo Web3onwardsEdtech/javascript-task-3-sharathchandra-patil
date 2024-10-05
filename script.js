19223094171f2ee201f1d355ef233ab946cab23c
Sharathchandra <sharathchandra9204@gmail.com>
2024-10-05 15:34:58 +0530

    Task 3 submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+const button1=document.getElementById('changeColorButton');
+const box=document.getElementById('colorBox');
+button1.addEventListener('click',function(){
+    box.style.backgroundColor='black';
+});
